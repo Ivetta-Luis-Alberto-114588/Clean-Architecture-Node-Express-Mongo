81ed176fdb14d1cd542a263b03709163fd01ee18
114588-Ivetta luis <114588@tecnicatura.frc.utn.edu.ar>
2025-02-20 10:00:47 -0300

    modify jwt

--- src/configs/jwt.ts
+++ src/configs/jwt.ts
@@ -5,10 +5,10 @@ const JWT_SEED = envs.JWT_SEED
 
 export class JwtAdapter {
 
-    static async generateToken(payload: Object, duration?: '2h'): Promise<string | null>{
+    //el payload es la informacion que quiero guardar en el token
+    static async generateToken(payload: Object, duration: string = '2h'): Promise<string | null>{
 
-        return new Promise((resolve)=>{
-            
+        return new Promise((resolve)=>{            
             
             jwt.sign(payload, JWT_SEED, {expiresIn:duration}, (err, token)=>{
                 
@@ -19,6 +19,8 @@ export class JwtAdapter {
         })
     }
 
+
+    //aca valido el token y devuelvo el payload
     static validateToken<T>(token: string): Promise<T | null>  {
         return new Promise((resolve)=>{
             jwt.verify(token, JWT_SEED, (err, decoded)=>{
@@ -28,6 +30,6 @@ export class JwtAdapter {
                 resolve(decoded as T)
 
             })
-        })
+        }) 
     }
 }
